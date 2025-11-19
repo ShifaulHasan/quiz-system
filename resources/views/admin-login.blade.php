c96f12bb09696a40a6c7c58a424e9d6678ffe782
Shifaul Hasan <shifaulhasantaship@gmail.com>
2025-11-19 13:14:50 +0600

    Validation and session

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -14,6 +14,10 @@
         <h2 class="text-2xl font-semibold text-center text-gray-800 mb-6">
             Admin Login
         </h2>
+         @error('user')
+                    <div class="text-red-500">{{$message}}</div>
+                    @enderror
+                    
 
         <form action="/admin-login" method="POST" class="space-y-4">
             @csrf
@@ -25,8 +29,13 @@
                     type="text" 
                     name="name"
                     placeholder="admin"
-                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
-                >
+                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+
+                    @error('name')
+                    <div class="text-red-500">{{$message}}</div>
+                    @enderror
+                    
+                
             </div>
 
             <!-- Password -->
@@ -38,6 +47,9 @@
                     placeholder="•••"
                     class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"
                 >
+                @error('password')
+                <div class="text-red-500">{{$message}}</div>
+                @enderror
             </div>
 
             <!-- Login Button -->
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <h1>Hello Admin{{$name}}</h1>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>Admin Dashboard</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <nav class="bg-white shadow-md px-4 py-3">
+        <div class="flex justify-between item-center">
+        <div class="text-2xl text-gray-700 hover:text-blue-500 cursor-pointer">
+            Quiz System
+        </div>
+        <div class=" space-x-6">
+            <a class="text-gray-700 hover:text-blue-500">Categories</a>
+            <a class="text-gray-700 hover:text-blue-500"href="">Quiz</a>
+            <a class="text-gray-700 hover:text-blue-500"href="">Welcome{{$name}}</a>
+            <a class="text-gray-700 hover:text-blue-500"href="">Login</a>
+            </div>
+        </div>
+    </nav>
+    
+</body>
+</html>
